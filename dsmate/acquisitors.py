bc8e646cd43002c689c8ab96f019d2b8d00dd252
Andre Sionek <andresionek91@gmail.copm>
2019-07-18 21:51:22 -0300

    add connection to Redshift

--- dsmate/acquisitors.py
+++ dsmate/acquisitors.py
@@ -1,4 +1,4 @@
-from dsmate.connections import Athena
+from dsmate.connections import Athena, Redshift
 import pandas as pd
 from dsmate.version_control import S3VersionControl
 
@@ -16,16 +16,16 @@ class AthenaAcquisitor:
     )
     """
 
-    def __init__(self, sql, params):
+    def __init__(self, sql, **kwargs):
         self.sql = sql
         self.filename = None
-        self.params = params
+        self.kwargs = kwargs
 
     def _execute_query(self):
         """
         Execute query and save data to S3
         """
-        athena = Athena(**self.params)
+        athena = Athena(**self.kwargs)
         self.filename = athena.query_to_s3(self.sql)
         return self.filename
 
@@ -33,7 +33,7 @@ class AthenaAcquisitor:
         """
         Load dataframe from S3.
         """
-        athena = Athena(**self.params)
+        athena = Athena(**self.kwargs)
         dataframe = pd.read_csv(athena.s3_path + '/' + self.filename)
         return dataframe
 
@@ -42,7 +42,55 @@ class AthenaAcquisitor:
         Run acquisition and version control.
         :return: Tuple of dicts with latest acquisitor and latest dataframe
         """
-        version_control = S3VersionControl(step='data-acquisition', **self.params)
+        version_control = S3VersionControl(step='data-acquisition', **self.kwargs)
+        self._execute_query()
+        dataframe = self._load_dataframe()
+        version_control.save(object_type='dataframe', data=dataframe)
+        version_control.save(object_type='acquisitor', data=self)
+
+        return version_control.load_latest('acquisitor'), version_control.load_latest('dataframe')
+
+
+class RedshiftAcquisitor:
+    """
+    Executes a query on AWS Redshift, saves to S3 and loads a Pandas Dataframe.
+
+    Provide a SQL query and the following dict of params:
+    PARAMS = dict(
+        database='database_name',
+        bucket_name='bucket_name',
+        project_name='project_name',
+        region='region_name'
+    )
+    """
+
+    def __init__(self, sql, **kwargs):
+        self.sql = sql
+        self.filename = None
+        self.kwargs = kwargs
+
+    def _execute_query(self):
+        """
+        Execute query and save data to S3
+        """
+        redshift = Redshift(**self.kwargs)
+        self.filename = redshift.query_to_s3(self.sql)
+        return self.filename
+
+    def _load_dataframe(self):
+        """
+        Load dataframe from S3.
+        """
+        redshift = Redshift(**self.kwargs)
+        dataframe = pd.read_csv('s3://' + redshift.s3_path + '/' + self.filename)
+        return dataframe
+
+    def run(self):
+        """
+        Run acquisition and version control.
+        :return: Tuple of dicts with latest acquisitor and latest dataframe
+        """
+        version_control = S3VersionControl(step='data-acquisition', **self.kwargs)
         self._execute_query()
         dataframe = self._load_dataframe()
         version_control.save(object_type='dataframe', data=dataframe)
--- dsmate/connections.py
+++ dsmate/connections.py
@@ -1,6 +1,11 @@
 import boto3
 import re
 import time
+import psycopg2
+import pandas as pd
+import os
+from io import StringIO
+import uuid
 
 
 class Athena:
@@ -52,3 +57,39 @@ class Athena:
             time.sleep(6)
 
         return False
+
+
+class Redshift:
+    def __init__(self, bucket_name, project_name, **kwargs):
+        db_config = {'dbname': os.environ.get('REDSHIFT_DBNAME', 'database'),
+                     'host': os.environ.get('REDSHIFT_HOST', 'localhost'),
+                     'password': os.environ.get('REDSHIFT_PASSWORD', ''),
+                     'port': os.environ.get('REDSHIFT_PORT', '5439'),
+                     'user': os.environ.get('REDSHIFT_USER', 'user')}
+
+        self.connection = psycopg2.connect(**db_config)
+        self.cursor = self.connection.cursor()
+        self.bucket_name = bucket_name
+        self.project_name = project_name
+        self.s3_path = self.project_name + '/data-acquisition/csv'
+        self.s3_resource = boto3.resource('s3')
+
+    def query(self, sql):
+        """
+        Execute the provided SQL against AWS Redshift
+        """
+
+        return pd.read_sql(sql=sql, con=self.connection)
+
+    def query_to_s3(self, sql):
+        """
+        Save the query results to S3 as CSV file
+        """
+        df = self.query(sql)
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer, index=False)
+
+        filename = str(uuid.uuid4()) + '.csv'
+        self.s3_resource.Object(self.bucket_name, self.s3_path + '/' + filename).put(Body=csv_buffer.getvalue())
+
+        return filename
